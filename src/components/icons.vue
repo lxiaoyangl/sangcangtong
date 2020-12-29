32ab9e7fcf04928c46846bec779873e4c1f3bf7f
luoyh <237049279@qq.com>
2020-12-29 15:12:11 +0800

    微调样式

--- src/components/icons.vue
+++ src/components/icons.vue
@@ -12,6 +12,20 @@
       <i class="nav-icon nav_watch"></i>
     </div> -->
    <div class="ico-list" id="icoList" @click="fun">
+      <i class="iconfont-ali icon-ali-mall"></i>
+      <i class="el-icon-s-check"></i>
+      <i class="iconfont-ali icon-ali-wuzicangku"></i>
+      <i class="iconfont-ali icon-ali-icon_function_ruku"></i>
+      <i class="iconfont-ali icon-ali-goodswhoutStock"></i>
+      <i class="iconfont-ali icon-ali-yonghuguohu-"></i>
+      <i class="iconfont-ali icon-ali-yunshu"></i>
+      <i class="iconfont-ali icon-ali-dingdan"></i>
+      <i class="iconfont-ali icon-ali-cheliangrenzheng"></i>
+      <i class="iconfont-ali icon-ali-sijirenzheng"></i>
+      <i class="iconfont-ali icon-ali-gongzuotai-querenbeijian"></i>
+      <i class="iconfont-ali icon-ali-rongzi"></i>
+      <i class="iconfont-ali icon-ali-woyaorongzi00"></i>
+
       <i class="fa fa-address-book"></i>
       <i class="fa fa-address-book-o" aria-hidden="true"></i>
       <i class="fa fa-address-card" aria-hidden="true"></i>
